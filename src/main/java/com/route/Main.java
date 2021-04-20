f7ae1f4615a76b113194f0de3881299ea7976009
kelkesjoose <kaha-kelly@hotmail.com>
2021-04-20 09:10:25 +0200

    Update dotenv werkend gemaakt

--- src/main/java/com/route/Main.java
+++ src/main/java/com/route/Main.java
@@ -1,7 +1,15 @@
 package com.route;
+import io.github.cdimascio.dotenv.Dotenv;
+
+
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println(System.getenv("TEST_ENV"));
+    public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().load();
+
+        System.out.println(dotenv.get("MY_ENV_VAR1"));
+        System.out.println(dotenv.get("MY_EVV_VAR2"));
+        System.out.println(dotenv.get("TEST_ENV"));
+        }
     }
-}
