9e3934e14eb282bdb0da76c17f14450d666cc1d1
Your Name <icecold2000@hotmail.nl>
2021-04-23 13:08:24 +0200

    Add CalculateRoute

---
+++ src/main/java/com/google/CalculateRoute.java
@@ -0,0 +1,51 @@
+package com.google;
+
+import com.entity.Customer;
+import com.entity.Order;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.io.IOException;
+import java.util.Set;
+
+public class CalculateRoute {
+    private final Dotenv env;
+
+    public CalculateRoute() {
+        env = Dotenv.configure().load();
+    }
+
+    public DirectionsRoute RouteMaker(Set<Order> orders) {
+        GeoApiContext context = new GeoApiContext.Builder().apiKey(env.get("SECRET_GOOGLE_API_KEY")).maxRetries(10).queryRateLimit(10).build();
+        DirectionsApiRequest directionsApiRequest =
+                DirectionsApi.newRequest(context)
+                        .origin(env.get("GOOGLE_DIRECTION_DEPOT")).destination(env.get("GOOGLE_DIRECTION_DEPOT"))
+                        .optimizeWaypoints(true).waypoints(ConvertRoute(orders));
+        try {
+            DirectionsResult directionsResult = directionsApiRequest.await();
+            return directionsResult.routes[0];
+
+        } catch (ApiException | InterruptedException | IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public String[] ConvertRoute(Set<Order> orders) {
+        String[] addresses = new String[orders.size()];
+        int index = 0;
+
+        for (Order o : orders) {
+            String a = (o.getCustumer().toString());
+            addresses[index] = a;
+            index = index + 1;
+        }
+
+        return addresses;
+    }
+}
