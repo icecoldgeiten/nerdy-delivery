f8814ec7b1334de3167be256a517cd02da7bc8df
Your Name <icecold2000@hotmail.nl>
2021-04-23 14:40:55 +0200

    Add Route list

--- src/main/java/org/openjfx/RouteController.java
+++ src/main/java/org/openjfx/RouteController.java
@@ -2,41 +2,40 @@ package org.openjfx;
 
 import com.dao.RouteDao;
 import com.entity.Route;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.StackPane;
-import javafx.util.Callback;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class RouteController {
     public Button back;
     public Button done;
     public Button new_route;
+    public ListView<String> list;
 
-    public RouteController() {
-        RouteDao route = new RouteDao();
-        ListView<Route> list = new ListView<>();
-        ObservableList<Route> observableList = FXCollections.observableList(route.getAllRoutes());
+    public RouteDao route;
 
-        list.setItems(observableList);
-        list.setCellFactory(new Callback<>() {
-            @Override
-            public ListCell<Route> call(ListView<Route> p) {
-                return new ListCell<>() {
-                    @Override
-                    protected void updateItem(Route t, boolean bln) {
-                        super.updateItem(t, bln);
-                        if (t != null) {
-                            setText(t.getId() + ":" + t.getDriver());
-                        }
-                    }
-                };
-            }
-        });
+    @FXML
+    public void initialize() {
+        route = new RouteDao();
+        updateRoutes();
+    }
+
+    public void updateRoutes() {
+        list.getItems().clear();
+        loadRoutes();
+    }
+
+    public void loadRoutes() {
+        List<Route> routes = route.getAllRoutes();
+
+        for (Route r : routes) {
+            list.getItems().add(r.toString());
+        }
     }
 
     @FXML
