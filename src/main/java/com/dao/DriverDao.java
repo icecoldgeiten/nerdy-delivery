9778453a7ff8b2e8583e86b5e33f295c56d48272
Your Name <icecold2000@hotmail.nl>
2021-05-27 10:38:39 +0200

    Add driver Dao

--- src/main/java/com/dao/DriverDao.java
+++ src/main/java/com/dao/DriverDao.java
@@ -30,7 +30,6 @@ public class DriverDao {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        em.getTransaction().commit();
         em.close();
         return null;
     }
@@ -39,9 +38,7 @@ public class DriverDao {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
         try {
-            em.getTransaction().begin();
-            List<Driver> drivers = em.createQuery("from Driver where active = 1", Driver.class).getResultList();
-            em.getTransaction().commit();
+            List<Driver> drivers = em.createQuery("from Driver", Driver.class).getResultList();
             em.close();
             return filterDrivers(drivers, date, timeslot);
         } catch (Exception e) {
@@ -71,6 +68,7 @@ public class DriverDao {
         String rp = randomPassword(10);
         try {
             driver = new Driver();
+
             driver.setName(name);
             driver.setInserts(ins);
             driver.setSirname(sn);
@@ -79,6 +77,7 @@ public class DriverDao {
             driver.setUsername(un);
             driver.setPassword(AES256.encrypt(rp));
             driver.setActive(true);
+
             em.persist(driver);
             em.getTransaction().commit();
 
@@ -138,16 +137,16 @@ public class DriverDao {
 
     public boolean validate(String username, String password) {
         EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
         try {
-            em.getTransaction().begin();
             Driver driver = em.createQuery("from Driver D where D.username = :username", Driver.class).setParameter("username", username).getSingleResult();
             if (driver != null && driver.getPassword().equals(AES256.encrypt(password))) {
                 LogedinDriver = driver;
                 return true;
             }
             em.getTransaction().commit();
-        } catch (NoResultException e) {
-            System.out.println("No user");
+        } catch (NoResultException e) { ;
+            e.printStackTrace();
         }
         em.close();
         return false;
@@ -184,4 +183,5 @@ public class DriverDao {
             System.out.println(e);
         }
     }
+
 }
